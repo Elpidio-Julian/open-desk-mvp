33b9fd986ee8712b8fa31e0ad463dd5f605e5060
Elpidio Julian <elpidiojulian@gmail.com>
2025-01-28 12:01:49 -0600

    Preparing to merge with main, its time to test the deploy with the improved prod db

--- supabase-old/migrations/00_create_types.sql
+++
@@ -1,6 +0,0 @@
--- Application roles enum
-CREATE TYPE app_role AS ENUM ('admin', 'agent', 'customer');
-
--- Create ENUMs for ticket statuses and priorities
-CREATE TYPE ticket_status AS ENUM ('open', 'in_progress', 'waiting_on_customer', 'resolved', 'closed');
-CREATE TYPE ticket_priority AS ENUM ('low', 'medium', 'high', 'urgent');
